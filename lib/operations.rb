74bdd8ffd3232b4ebc692419b45253583415d5ad
Ryne Stajcar <rynostajcar@gmail.com>
2017-12-29 17:49:30 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	if speed > 60 || speed < 40 ? true : false
+	if speed > 60 || speed < 40
+		return true
+	elsif speed.between?(40, 60)
+		return false
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
